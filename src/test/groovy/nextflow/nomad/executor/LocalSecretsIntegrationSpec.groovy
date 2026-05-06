21e64045e1c42b3c01624adda4dd40de8a234c7b
Abhinav Sharma <abhi18av@outlook.com>
2026-05-06 13:59:07 +0200

    chore(license): add Incremental Steps Software Solutions OÜ copyright line to existing source headers
    Adds a third copyright line to every Groovy source file under src/main and
    src/test:
    
        * Copyright 2023-, Stellenbosch University, South Africa
        * Copyright 2024, Evaluacion y Desarrollo de Negocios, Spain
        * Copyright 2026-, Incremental Steps Software Solutions OÜ
    
    Incremental Steps is an additional copyright holder, not a replacement —
    the historical Stellenbosch + Evaluacion lines are preserved.
    
    License terms (Apache 2.0) and COPYING file unchanged.
    46 pre-existing source files touched, header-only edits, all tests still
    passing.

--- src/test/groovy/nextflow/nomad/executor/LocalSecretsIntegrationSpec.groovy
+++ src/test/groovy/nextflow/nomad/executor/LocalSecretsIntegrationSpec.groovy
@@ -1,6 +1,7 @@
 /*
  * Copyright 2023-, Stellenbosch University, South Africa
  * Copyright 2024, Evaluacion y Desarrollo de Negocios, Spain
+ * Copyright 2026-, Incremental Steps Software Solutions OÜ
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
