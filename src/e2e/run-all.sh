8082d118b429bbd293d18fee38637847d73a1b91
Jorge <jagedn@gmail.com>
2026-06-04 08:28:10 +0200

    detect failsafe version (#130)
    Signed-off-by: Jorge Aguilera <jagedn@gmail.com>

--- src/e2e/run-all.sh
+++ src/e2e/run-all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+export NXF_SYNTAX_PARSER=v1
 sudo rm -rf /tmp/nomad/nomad_temp/scratchdir/tests/
-NXF_ASSETS=/tmp/nomad/nomad_temp/scratchdir/assets ./nf-test test
+NXF_ASSETS=/tmp/nomad/nomad_temp/scratchdir/assets ./nf-test test $@
